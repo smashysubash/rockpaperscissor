7f5dafaef9c8a46aa2742fd035b45e7ba4597d94
Smashy <rocksubash2003@gmail.com>
2022-05-03 21:22:01 +0530

    Creating files

---
+++ file1.js
@@ -0,0 +1,55 @@
+const play_options = ["rock","paper","scissor"];
+let player_point , computer_point;
+
+
+//function to pick a random play_option as a computer
+function computerPlay()
+{
+    return play_options[Math.floor(Math.random()*3)];
+}
+
+
+//function to decide the round
+function singleRoundPlay(playerSelection,computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection=="rock" && computerSelection=="paper"){
+        computer_point++;
+        return "You Lose! Paper beats Rock";
+    }else if(playerSelection=="rock" && computerSelection=="scissor"){
+        player_point++;
+        return "You Win! Rock beats Scissor";
+    }else if(playerSelection===computerSelection){
+        return "Its Tie! ";
+    }else if(playerSelection=="paper" && computerSelection=="scissor"){
+        computer_point++;
+        return "You Lose! Scissor beats Paper";
+    }else if(playerSelection=="paper" && computerSelection=="rock"){
+        player_point++;
+        return "You Win! Paper beats Rock";
+    }else if (playerSelection=="scissor" && computerSelection=="paper"){
+        player_point++;
+        return "You Win! Scissor beats Paper";
+    }else if(playerSelection=="scissor" && computerSelection=="rock"){
+        computer_point++;
+        return "You Lose! Rock beats Scissor";
+    }
+}
+
+function game(){
+    player_point=0;
+    computer_point=0;
+    for(let i=0;i<5;i++){
+        let playerOpt=prompt("Enter your option:");
+        singleRoundPlay(playerOpt,computerPlay());
+    }
+    if(player_point==computer_point){
+        return "Its Tie!!! You:"+player_point.toString() + " Computer " + computer_point.toString();
+    }
+    else if(player_point>computer_point){
+        return "You Win!!! You:"+player_point.toString() + " Computer " + computer_point.toString();
+    }
+    else if(player_point<computer_point){
+        return "You Lose!!! You:"+player_point.toString() + " Computer " + computer_point.toString();
+    }
+}
